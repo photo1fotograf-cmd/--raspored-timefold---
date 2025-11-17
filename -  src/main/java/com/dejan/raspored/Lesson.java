079f1e924cd21927beb7828307a8dfc671770140
photo1fotograf-cmd <photo1fotograf@gmail.com>
2025-11-17 18:14:38 +0100

    Refactor Lesson class with constructor and getters/setters

--- -  src/main/java/com/dejan/raspored/Lesson.java
+++ -  src/main/java/com/dejan/raspored/Lesson.java
@@ -1,14 +1,56 @@
+package com.dejan.raspored;
+
 import ai.timefold.solver.core.api.domain.entity.PlanningEntity;
 import ai.timefold.solver.core.api.domain.variable.PlanningVariable;
 
 @PlanningEntity
 public class Lesson {
-    private Teacher nastavnik;
-    private String odeljenje;
+
     private String predmet;
+    private String odeljenje;
+    private Teacher nastavnik;
 
     @PlanningVariable(valueRangeProviderRefs = "timeslotRange")
     private Timeslot timeslot;
 
-    // Konstruktor, getteri, setteri
+    public Lesson() {
+    }
+
+    public Lesson(String predmet, String odeljenje, Teacher nastavnik) {
+        this.predmet = predmet;
+        this.odeljenje = odeljenje;
+        this.nastavnik = nastavnik;
+    }
+
+    public String getPredmet() {
+        return predmet;
+    }
+
+    public void setPredmet(String predmet) {
+        this.predmet = predmet;
+    }
+
+    public String getOdeljenje() {
+        return odeljenje;
+    }
+
+    public void setOdeljenje(String odeljenje) {
+        this.odeljenje = odeljenje;
+    }
+
+    public Teacher getNastavnik() {
+        return nastavnik;
+    }
+
+    public void setNastavnik(Teacher nastavnik) {
+        this.nastavnik = nastavnik;
+    }
+
+    public Timeslot getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(Timeslot timeslot) {
+        this.timeslot = timeslot;
+    }
 }
